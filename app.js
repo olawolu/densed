4a7c353cacdb49fd334cba6c9becaa2a21f3dcc9
Gbenga <omg.tosine@gmail.com>
2021-04-19 00:25:06 +0100

    fix: removed protocol from url

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use("/graphiql", graphqlHTTP({
 }));
 
 app.get('/:hash', async (req, res) => {
-    const short = req.protocol + "://"+ req.get('host') + req.originalUrl
+    const short = req.get('host') + req.originalUrl
     console.log(short);
     const urlEntry = await UrlEntry.findOne({ shortUrl: short })
     if (urlEntry) {
