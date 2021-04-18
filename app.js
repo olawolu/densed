54ddb40c2d2fd799195e6df054ca0fde21d55e14
Gbenga <omg.tosine@gmail.com>
2021-04-18 22:34:41 +0100

    feature: api server logic

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const graphqlSchema = require("./graphql/schema")
+const graphqlResolvers = require("./graphql/resolver")
+const mongoose = require("mongoose")
+const UrlEntry = require("./models/urls")
+
+const app = express()
+
+app.use("/graphiql", graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true,
+}));
+
+app.get('/:hash', async (req, res) => {
+    const short = req.get('host') + req.originalUrl
+    const urlEntry = await UrlEntry.findOne({ shortUrl: short })
+    if (urlEntry) {
+        return res.redirect(urlEntry.originalUrl)
+    }
+})
+
+const mongouri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@kawura.1u6zd.gcp.mongodb.net/${process.env.MONGO_DB}?retryWries=true&w=majority`
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+mongoose
+    .connect(mongouri, options)
+    .then(() => app.listen(process.env.PORT, console.log(`Server is running on port ${process.env.PORT}`)))
+    .catch(error => {
+        throw error
+    })
