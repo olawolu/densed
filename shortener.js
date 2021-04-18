1ca518c7942e0d1690447f515abbe9d9420debd2
Gbenga <omg.tosine@gmail.com>
2021-04-19 00:02:23 +0100

    fix: removed base64 encoding

--- shortener.js
+++ shortener.js
@@ -1,5 +1,5 @@
 const sha256 = require("crypto-js/sha256")
-const base64 = require('crypto-js/enc-base64')
+const hex = require('crypto-js/enc-hex')
 // Requirements:
 //  short url must consist of the host and a 6 character string
 //  multiple entry of the same url should not generate the same short url
@@ -12,9 +12,8 @@ module.exports = {
     hashUrl: (url) => {
         var result = [];
         // generate sha256 hash
-        var urlHash = sha256(url)
-        var digest = base64.stringify(urlHash)
-
+        var digest = sha256(url).toString(hex)
+        
         for (var i = 0; i < 6; i++) {
             result.push(digest.charAt(Math.floor(Math.random() *
                 digest.length)));
