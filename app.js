44b062e7a2b414351442819573a49dbd6df73e9f
Gbenga <omg.tosine@gmail.com>
2021-04-19 10:50:02 +0100

    .

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const graphqlSchema = require("./graphql/schema")
 const graphqlResolvers = require("./graphql/resolver")
+const redirect = require("./routes/redirect")
 const mongoose = require("mongoose")
-const UrlEntry = require("./models/urls")
 
 const app = express()
 
@@ -12,17 +12,17 @@ app.use("/graphiql", graphqlHTTP({
     rootValue: graphqlResolvers,
     graphiql: true,
 }));
-
-app.get('/:hash', async (req, res) => {
-    const base_url = process.env.BASE
-    const short = base_url + req.originalUrl
-    console.log(req.originalUrl);
-    console.log(short);
-    const urlEntry = await UrlEntry.findOne({ shortUrl: short })
-    if (urlEntry) {
-        return res.redirect(urlEntry.originalUrl)
-    }
-})
+redirect(app)
+// app.get('/:hash', async (req, res) => {
+//     const base_url = process.env.BASE
+//     const short = base_url + req.originalUrl
+//     console.log(req.originalUrl);
+//     console.log(short);
+//     const urlEntry = await UrlEntry.findOne({ shortUrl: short })
+//     if (urlEntry) {
+//         return res.redirect(urlEntry.originalUrl)
+//     }
+// })
 
 const mongouri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@kawura.1u6zd.gcp.mongodb.net/${process.env.MONGO_DB}?retryWries=true&w=majority`
 const options = {
